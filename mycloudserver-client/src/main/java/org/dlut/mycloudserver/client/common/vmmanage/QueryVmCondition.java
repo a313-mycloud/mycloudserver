a17362af55556cc1b884d0e754a47cdb0308a5ca
ZhenShiErGe <895990111@qq.com>
2015-09-24 21:22:29 +0800

    修改bug

--- mycloudserver-client/src/main/java/org/dlut/mycloudserver/client/common/vmmanage/QueryVmCondition.java
+++ mycloudserver-client/src/main/java/org/dlut/mycloudserver/client/common/vmmanage/QueryVmCondition.java
@@ -77,6 +77,20 @@ public class QueryVmCondition implements Serializable {
      * 是否是共有的模板虚拟机
      */
     private Boolean           isPublicTemplate;
+    
+    /*
+     * 记录当前文件系统中存储的镜像版本，新创建的版本号为0，每次更新加1
+     */
+    private Integer imageVersion;
+    /*
+     * 记录上次该像像运行的主机号，初次创建为-1
+     */
+    private Integer  lastHostId;
+    /*
+     * 标记当前镜像是否可读，默认为1
+     */
+    private Integer isCanRead;
+    
 
     private int               offset           = 0;
 
@@ -193,5 +207,31 @@ public class QueryVmCondition implements Serializable {
     public void setIsPublicTemplate(Boolean isPublicTemplate) {
         this.isPublicTemplate = isPublicTemplate;
     }
+
+	public Integer getImageVersion() {
+		return imageVersion;
+	}
+
+	public void setImageVersion(Integer imageVersion) {
+		this.imageVersion = imageVersion;
+	}
+
+	public Integer getLastHostId() {
+		return lastHostId;
+	}
+
+	public void setLastHostId(Integer lastHostId) {
+		this.lastHostId = lastHostId;
+	}
+
+	public Integer getIsCanRead() {
+		return isCanRead;
+	}
+
+	public void setIsCanRead(Integer isCanRead) {
+		this.isCanRead = isCanRead;
+	}
+    
+    
 
 }
